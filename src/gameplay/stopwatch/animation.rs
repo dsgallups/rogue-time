f8299aa6a98ee2baf2c59840735cde2cc0553516
Daniel Gallups <dsgallups@protonmail.com>
2025-05-04 11:42:44 -0400

    feat: scaffold stopwatch animation plugin

---
+++ src/gameplay/stopwatch/animation.rs
@@ -0,0 +1,45 @@
+use bevy::prelude::*;
+
+use crate::{asset_tracking::LoadResource, gameplay::animation::AnimationPlayers};
+
+pub fn plugin(app: &mut App) {
+    app.load_resource::<StopwatchAnimationAssets>();
+    app.register_type::<StopwatchAnimationAssets>();
+    app.register_type::<StopwatchAnimations>();
+}
+#[derive(Resource, Asset, Clone, Reflect)]
+#[reflect(Resource)]
+struct StopwatchAnimationAssets {
+    #[dependency]
+    pub click_animation: Handle<AnimationClip>,
+    #[dependency]
+    pub ticking_animation: Handle<AnimationClip>,
+}
+
+impl FromWorld for StopwatchAnimationAssets {
+    fn from_world(world: &mut World) -> Self {
+        let assets = world.resource::<AssetServer>();
+        //TODO: probably not the right path
+        Self {
+            click_animation: assets
+                .load(GltfAssetLabel::Animation(0).from_asset("scenes/stopwatch.glb")),
+            ticking_animation: assets
+                .load(GltfAssetLabel::Animation(1).from_asset("scenes/stopwatch.glb")),
+        }
+    }
+}
+
+#[derive(Component, Debug, Reflect)]
+#[reflect(Component)]
+struct StopwatchAnimations {
+    click: AnimationNodeIndex,
+    ticking: AnimationNodeIndex,
+}
+// we will have a trigger will then trigger this on the StopWatch component being inserted...
+// or maybe this happens automagically with the animation plugin via link_animation_player
+pub(super) fn setup_stopwatch_animation(
+    trigger: Trigger<OnAdd, AnimationPlayers>,
+    q_anim_players: Query<&AnimationPlayers>,
+    mut commands: Commands,
+) {
+}
