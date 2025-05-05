8fcbf6181a288c2362947bf17b8c9f599fc894ed
Daniel Gallups <dsgallups@protonmail.com>
2025-05-05 06:11:15 -0400

    feat: simple stopwatch ui

--- src/gameplay/ui/mod.rs
+++ src/gameplay/ui/mod.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    color::palettes::css::{BLACK, RED, WHITE},
+    prelude::*,
+};
 
 use crate::{screens::Screen, theme::Containers};
 
@@ -11,10 +14,7 @@ pub fn plugin(app: &mut App) {
 }
 
 #[derive(Component)]
-pub struct InteractionText;
-
-#[derive(Component)]
-pub struct HealthBar;
+struct StopwatchTime;
 
 #[derive(Component)]
 pub struct GameUi;
@@ -23,5 +23,37 @@ fn spawn_game_ui(mut commands: Commands) {
     // this does nothing essentially
     commands
         .ui_root()
-        .insert((GameUi, Name::new("Game UI"), StateScoped(Screen::Gameplay)));
+        .insert((GameUi, Name::new("Game UI"), StateScoped(Screen::Gameplay)))
+        .with_children(|parent| {
+            let font = TextFont {
+                font_size: 20.,
+                ..default()
+            };
+            parent.spawn((
+                Node {
+                    flex_grow: 1.,
+                    align_items: AlignItems::End,
+                    ..default()
+                },
+                children![(
+                    Node {
+                        width: Val::Px(50.),
+                        height: Val::Px(80.),
+                        border: UiRect::all(Val::Px(10.)),
+                        margin: UiRect::all(Val::Px(20.)),
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    BorderColor(BLACK.into()),
+                    BackgroundColor(WHITE.into()),
+                    children![(
+                        StopwatchTime,
+                        Text::new("N/A"),
+                        font,
+                        TextColor(BLACK.into())
+                    )]
+                )],
+            ));
+        });
 }
