4b318fa16549f58868fa84b746aeb6955a285c33
Daniel Gallups <dsgallups@protonmail.com>
2025-05-05 06:49:29 -0400

    log distance to portal

--- src/gameplay/dev/portal.rs
+++ src/gameplay/dev/portal.rs
@@ -1,10 +1,14 @@
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
-use crate::gameplay::{player::Player, portal::Portal};
+use crate::gameplay::{GameSet, player::Player, portal::Portal};
 
 pub fn plugin(app: &mut App) {
     app.add_observer(debug_portal_insert);
+    app.add_systems(
+        Update,
+        determine_distance_from_portal.in_set(GameSet::Update),
+    );
 }
 
 ///Portals are sensors
@@ -28,5 +32,17 @@ fn debug_portaling(trigger: Trigger<OnCollisionStart>, player: Query<&Player>) {
         info!("Player collided with portal!");
     } else {
         info!("Player didn't collid with portal!");
+    }
+}
+
+fn determine_distance_from_portal(
+    player: Query<&Transform, With<Player>>,
+    portals: Query<&Transform, With<Portal>>,
+) {
+    let Ok(player) = player.single() else { return };
+    for portal in portals {
+        let diff = player.translation - portal.translation;
+
+        info!("\nDistance from portal: {}\nDiff: {}", diff.length(), diff);
     }
 }
