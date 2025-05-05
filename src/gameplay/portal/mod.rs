f3962f88adb0686bb7dd3950bb894e72e8604a31
Daniel Gallups <dsgallups@protonmail.com>
2025-05-05 06:53:42 -0400

    a collision was detected but it's not the player? strange.

--- src/gameplay/portal/mod.rs
+++ src/gameplay/portal/mod.rs
@@ -17,9 +17,11 @@ pub struct Portal {
 
 ///Portals are sensors
 fn insert_portal(trigger: Trigger<OnAdd, Portal>, mut commands: Commands) {
+    let collider =
+        ColliderConstructorHierarchy::new(ColliderConstructor::ConvexDecompositionFromMesh);
     commands
         .entity(trigger.target())
-        .insert((Sensor, CollisionEventsEnabled))
+        .insert((CollisionEventsEnabled, RigidBody::Static, collider))
         .observe(portal_me_elsewhere);
 }
 
@@ -32,7 +34,7 @@ fn portal_me_elsewhere(
 
     let event = trigger.event();
 
-    let Ok(mut player) = player.get_mut(event.collider) else {
+    let Ok(mut player) = player.get_mut(event.body.unwrap()) else {
         warn!("PME: not player");
         return;
     };
