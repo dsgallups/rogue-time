83cd1eb6ce84dedcbef0cf1b6d5ba55d45c193de
Daniel Gallups <dsgallups@protonmail.com>
2025-05-06 12:43:29 -0400

    cleanup

--- src/theme/mod.rs
+++ src/theme/mod.rs
@@ -1,10 +1,12 @@
 pub mod interaction;
 pub mod palette;
+#[cfg(feature = "native")]
 pub mod shader;
 pub mod widgets;
 
 use bevy::prelude::*;
 pub fn plugin(app: &mut App) {
-    //shader::plugin
-    app.add_plugins((interaction::plugin,));
+    app.add_plugins(interaction::plugin);
+    #[cfg(feature = "native")]
+    app.add_plugins(shader::plugin);
 }
