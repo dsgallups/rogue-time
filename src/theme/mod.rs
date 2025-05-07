3d1dc3cd5de80eef44924ed5b1f946c28c2baab9
Joseph Aghoghovbia <64096772+Jaghov@users.noreply.github.com>
2025-05-07 18:33:09 +0100

    Shaders (#15)
    * feat: swapped post process plugin out and added new marker components to distinguish 3d cameras
    
    * refactor: removed camera tags

--- src/theme/mod.rs
+++ src/theme/mod.rs
@@ -1,12 +1,12 @@
 pub mod interaction;
 pub mod palette;
-#[cfg(all(not(feature = "dev"), feature = "native"))]
+// #[cfg(all(not(feature = "dev"), feature = "native"))]
 pub mod shader;
 pub mod widgets;
 
 use bevy::prelude::*;
 pub fn plugin(app: &mut App) {
     app.add_plugins(interaction::plugin);
-    #[cfg(all(not(feature = "dev"), feature = "native"))]
+    // #[cfg(all(not(feature = "dev"), feature = "native"))]
     app.add_plugins(shader::plugin);
 }
