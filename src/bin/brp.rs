ea5dc138cf573a0cdc356613c72ad78f6427d852
Daniel Gallups <dsgallups@protonmail.com>
2025-05-09 13:07:50 -0400

    fix: update type registry

--- src/bin/brp.rs
+++ src/bin/brp.rs
@@ -4,9 +4,7 @@ use rogue_time::*;
 // this bin is used when reflected types have changed. It won't actually instantiate levels, and not kick off
 // other things that could be potentially erroneous
 fn main() {
-    let mut app = App::new();
-
-    app.add_plugins(AppPlugin { load_level: false });
-
-    app.run();
+    App::new()
+        .add_plugins(AppPlugin { load_level: false })
+        .run();
 }
--- src/bin/game.rs
+++ src/bin/game.rs
@@ -1,10 +1,5 @@
 use bevy::prelude::*;
 use rogue_time::*;
-// this bin is used when reflected types have changed
 fn main() {
-    let mut app = App::new();
-
-    app.add_plugins(AppPlugin::default());
-
-    app.run();
+    App::new().add_plugins(AppPlugin::default()).run();
 }
